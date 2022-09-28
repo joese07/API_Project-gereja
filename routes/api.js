f9533af81e6aff9ef4111b2b68585dad692d4445
Yohanes Joese <fransasisi7@gmail.com>
2022-09-28 23:21:56 +0700

    delete restrict

--- routes/api.js
+++ routes/api.js
@@ -5,14 +5,14 @@ const restrict = require("../middlewares/restrict");
 
 router.post("/auth/login", authController.login);
 router.post("/auth/register", authController.register);
-router.get("/auth/user", authController.index);
+router.get("/auth/user", restrict, authController.index);
 router.get("/auth/home", restrict, authController.whoami);
 
 //sabda-padua
 router.get("/sabda-padua", sabdaPaduaController.index);
 router.get("/sabda-padua/:id", sabdaPaduaController.show);
-router.post("/sabda-padua", sabdaPaduaController.store);
-router.put("/sabda-padua/:id", sabdaPaduaController.update);
-router.delete("/sabda-padua/:id", sabdaPaduaController.destroy);
+router.post("/sabda-padua", restrict, sabdaPaduaController.store);
+router.put("/sabda-padua/:id", restrict, sabdaPaduaController.update);
+router.delete("/sabda-padua/:id", restrict, sabdaPaduaController.destroy);
 
 module.exports = router;
